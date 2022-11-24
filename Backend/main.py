9bd642e1033f899afbbc467ba8618d1df46a6362
hazelnope <nopnprii@gmail.com>
2022-11-24 14:57:01 +0700

    Backend and Pamika data format
    Co-authored-by: Boonsak Surisarn <Zeedneo@users.noreply.github.com>

--- Backend/main.py
+++ Backend/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+import pandas as pd
 
 app = FastAPI()
+df_sample_data = pd.read_csv('../Preprocess/data/Sample_data1.csv')
 
 
 @app.get("/")
 def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/get_flight")
+def get_flight():
+
     return {"Hello": "World"}
