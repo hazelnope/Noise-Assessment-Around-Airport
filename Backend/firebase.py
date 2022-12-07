ad1c5922d963cc1cc16fb395e9641f3a5bcd230c
hazelnope <nopnprii@gmail.com>
2022-12-07 17:21:57 +0700

    dict interpolate API, test add data in firebase
    Co-authored-by: Boonsak Surisarn <Zeedneo@users.noreply.github.com>

--- Backend/firebase.py
+++ Backend/firebase.py
@@ -1,18 +1,46 @@
 
 import firebase_admin
 from firebase_admin import credentials
+from firebase_admin import firestore
+import pickle
 
-import os 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-print(dir_path)
-dir_list = os.listdir(dir_path)
- 
-print("Files and directories in '", dir_path, "' :")
- 
-# prints all files
-print(dir_list)
+import pandas as pd
 
 print()
-cred = credentials.Certificate('serviceAccount.json')
-firebase_admin.initialize_app(cred)
+cred = credentials.Certificate('Backend/serviceAccount.json')
+app = firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+doc_ref = db.collection(u'dd-mm-yy').document(u'Day').collection(u'flight_ID')
+# doc_ref = db.collection(u'users')
+# doc_ref = db.collection(u'Date').document(u'Day').collection(u'Arrivals').document(u'alovelace')
+# doc_ref.set({
+#     u'first': u'Ada',
+#     u'last': u'Lovelace',
+#     u'born': 1815
+# })
+
+
+
+df = pd.read_csv('Preprocess/data/Sample_data3.csv')
+postdata = df.to_dict('records')
+list(map(lambda x: doc_ref.add(x), postdata))
+# print(postdata)
+# doc_ref.push(postdata)
+
+
+
+# def batch_data(iterable, n=1):
+#     l = len(iterable)
+#     for ndx in range(0, l, n):
+#         yield iterable[ndx:min(ndx + n, l)]
+
+# def upload_file(df):
+#     file_name = f'test.csv'
+#     df.to_csv(file_name, index=False)
+
+#     return
+
+
 
