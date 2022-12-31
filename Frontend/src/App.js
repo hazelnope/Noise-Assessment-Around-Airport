b2ddfb4e592fc78f1e18d62576db0659b2e329a6
hazelnope <nopnprii@gmail.com>
2022-12-31 22:46:10 +0700

    navbar, app frontend
    Co-authored-by: Boonsak Surisarn <Zeedneo@users.noreply.github.com>

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { Component }  from 'react';
+import React, { Component, useState  }  from 'react';
 // import ReactEChartsCore from 'echarts-for-react/lib/core';
 
 // import * as echarts from 'echarts/core';
@@ -9,12 +9,41 @@ import 'mapbox-echarts'
 
 import Navbar from './navbar'
 import Map from './components/map';
+import Map2 from './components/map_Vfunc';
+
 
 function App() {
+  const [selectdate, setSelectdate] = useState('Hello there')
+  const [selecttime, setSelectTime] = useState('')
+
+  const handleDateChange = (newDate) => {
+    setSelectdate(newDate);
+  };
+
+  const handleTimeChange = (newTime) => {
+    setSelectTime(newTime);
+  };
+
   return (
     <div className="App">
-      <Navbar/>
-      <Map/>
+      <button onClick={() => (
+          setSelectdate("Reset")
+        )}>
+          Reset date
+        </button>
+
+      <button onClick={() => (
+          console.log(selectdate)
+        )}>
+          log date
+        </button>
+        
+      <Navbar selectdate = {selectdate}
+              handleDateChange = {handleDateChange}
+              handleTimeChange = {handleTimeChange}
+        /> 
+      <Map selectdate = {selectdate}/>
+      {/* <Map2/> */}
     </div>
   );
 }
--- Frontend/src/navbar.js
+++ Frontend/src/navbar.js
@@ -1,34 +1,52 @@
-import React, { Component }  from 'react';
+import React, { Component, useState }  from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
-function NavScrollExample() {
+
+
+function NavScrollExample(props) {
+
+  const handleDateClick = (eventKey) => {
+    props.handleDateChange(eventKey);
+  };
+
+  const handleTimeClick = (eventKey) => {
+    props.handleTimeChange(eventKey);
+  };
+
+  const options = [];
+  for (let i = 0; i < 8; i++) {
+    options.push(`${i < 4 ? '0' : ''}${i * 3}:00 - ${i < 3 ? '0' : ''}${i * 3 + 2}:59`);
+  }
+
   return (
     <Navbar bg="light" expand="lg">
       <Container fluid>
+        <button onClick={handleDateClick}>
+          Update date
+          </button>
         <Navbar.Brand href="#">Noise Assessment Around Airport</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav className="justify-content-end" style={{ width: "100%" }}>
-            <NavDropdown title="S E L E C T - D A T E " id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action1">10/12/2022</NavDropdown.Item>
-              <NavDropdown.Item href="#action2">15/12/2022</NavDropdown.Item>
-              <NavDropdown.Item href="#action3">20/12/2022</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">25/12/2022</NavDropdown.Item>
+            <NavDropdown title="S E L E C T - D A T E " id="navbarScrollingDropdown" onSelect={handleDateClick} >
+              <NavDropdown.Item href="#action1" eventKey="10/12/2022">10/12/2022</NavDropdown.Item>
+              <NavDropdown.Item href="#action2" eventKey="15/12/2022">15/12/2022</NavDropdown.Item>
+              <NavDropdown.Item href="#action3" eventKey="20/12/2022">20/12/2022</NavDropdown.Item>
+              <NavDropdown.Item href="#action4" eventKey="25/12/2022">25/12/2022</NavDropdown.Item>
+            </NavDropdown>
+            
+            <NavDropdown title="S E L E C T - T I M E "  onSelect={handleTimeClick} id="navbarScrollingDropdown">
+              {options.map((option) => (
+                <NavDropdown.Item eventKey={option} key={option}>
+                  {option}
+                </NavDropdown.Item>
+              ))}
             </NavDropdown>
 
-            <NavDropdown title="S E L E C T - T I M E " id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action1">00:00 - 02:59</NavDropdown.Item>
-              <NavDropdown.Item href="#action2">03:00 - 05:59</NavDropdown.Item>
-              <NavDropdown.Item href="#action3">06:00 - 08:59</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">09:00 - 11:59</NavDropdown.Item>
-              <NavDropdown.Item href="#action5">12:00 - 14:59</NavDropdown.Item>
-              <NavDropdown.Item href="#action6">15:00 - 17:59</NavDropdown.Item>
-              <NavDropdown.Item href="#action7">18:00 - 20:59</NavDropdown.Item>
-              <NavDropdown.Item href="#action8">21:00 - 23:59</NavDropdown.Item>
-            </NavDropdown>
+
           </Nav>
         </Navbar.Collapse>
       </Container>
