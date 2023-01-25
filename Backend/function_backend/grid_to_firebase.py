bc14374302935394c2d3ff61778de210670885a3
hazelnope <nopnprii@gmail.com>
2023-01-25 15:30:42 +0700

    grid to firebase
    Co-authored-by: Boonsak Surisarn <Zeedneo@users.noreply.github.com>

---
+++ Backend/function_backend/grid_to_firebase.py
@@ -0,0 +1,274 @@
+import os
+import glob
+import math
+import numpy as np
+import pandas as pd
+import firebase_admin
+import geopy.distance
+from scipy import integrate
+from firebase_admin import credentials
+from firebase_admin import firestore
+import sys
+import requests
+
+ 
+sys.path.append('./../Backend/')
+from key_value.token import apiKey, apiUrl, auth_header
+
+
+
+
+change_type = {
+    "A320"      : "737800",
+    "737-800"   : "737800",
+    "737-900"   : "737800"
+}
+
+mockup_api = ['AIQ311-1670716320-schedule-0614','AIQ3102-1670715240-schedule-0556']
+
+lat_start =  13.811475
+long_start = 100.505997
+L_dB_start = 0
+size = 50
+
+
+
+#----- read fix_point to get power setting -----#
+df_fix_point = pd.read_csv('./../Preprocess/data/ANP2.2_Default_fixed_point_profiles.csv',delimiter=';', skiprows=[0])
+df_fix_point = df_fix_point[df_fix_point['Stage Length'] == 1]
+df_fix_point = df_fix_point[df_fix_point['Op Type'] == "A"]
+
+#----- read NPD to get L(dB) by [power setting - distance]-----#
+npd = pd.read_csv('./../Preprocess/data/ANP2.2_NPD_data.csv',delimiter=';', skiprows=[0])
+
+
+
+#----- calculate distance from lat, long -----#
+def add_distance(lat1, long1, lat2, long2, h2):
+    position1 = (lat1,long1)
+    position2 = (lat2,long2)
+    a = geopy.distance.geodesic(position1, position2).ft
+    b = h2
+    b = b*100
+    c_square = a**2 + b**2
+    c = math.sqrt(c_square)
+    return c
+
+#----- function Single Soung Event -----#
+def function_L(sound) :
+    return 10**(sound / 10)
+
+def get_feet(distance):
+    feet = distance.split('_')[1]
+    return float(feet[:-2])
+
+def npd_inter_per_flight(df_param, npd_id):
+    npd_of_flight = npd[npd['NPD_ID'] == npd_id]
+    npd_of_flight = npd_of_flight[npd_of_flight['Noise Metric'] == 'LAmax']
+    npd_of_flight = npd_of_flight[npd_of_flight['Op Mode'] == 'A']
+    npd_of_flight.set_index('Power Setting', inplace=True)
+
+    npd_of_flight = npd_of_flight.reindex(npd_of_flight.index.union(df_param['Power Setting'].unique()))
+    npd_of_flight = npd_of_flight.interpolate(method='index',limit_direction='both',limit=200)
+    
+    npd_of_flight = npd_of_flight.drop(columns=['NPD_ID','Noise Metric','Op Mode']).T.reset_index()
+    npd_of_flight.rename(columns={'index':'distance'},inplace=True)
+
+    npd_of_flight['distance'] = npd_of_flight['distance'].apply(get_feet)
+    npd_of_flight.set_index('distance',inplace=True)
+    return npd_of_flight
+
+def add_powersetting_2_flight(df_param, df_fixpoint):
+    df__tmp = df_fixpoint.copy().groupby('TAS (kt)').max()
+    df__tmp = df__tmp.reindex(df__tmp.index.union(df_param.groundspeed.unique()))
+    df__tmp = df__tmp.interpolate(method='index',limit_direction='both',limit=200)
+    df__tmp = df__tmp.reindex(df_param.groundspeed.unique())
+    df__tmp.reset_index(drop=False, inplace=True)
+    df__tmp.rename(columns={'TAS (kt)':'groundspeed'}, inplace=True)
+    df__tmp = df__tmp[['groundspeed', 'Power Setting']]
+
+    df_param = df_param.merge(df__tmp, how='left', on='groundspeed')
+    
+    return df_param
+
+def create_observerer():
+    global lat_start, long_start, L_dB_start
+    observer = np.zeros((size,size,3))
+    for i in range(size) :
+        for j in range(size) :
+            observer[i][j] = lat_start, long_start, L_dB_start
+            long_start += 0.004
+        lat_start  += 0.004
+        long_start = 100.505997
+    lat_start =  13.811475    
+    return observer
+
+def Calculate_Grid(observer,df_param, npd_param):
+    for i in range(len(observer)):
+        for j in range(len(observer[i])): # j = [lat, long, L(dB)]
+            #----- copy df_param -----#
+            tmp = df_param.copy()
+            
+            #----- calculate distance every transection -----#
+            tmp_distance = []
+            tmp['distance'] = -1
+            
+            for index, row in tmp.iterrows():
+                distance = add_distance(observer[i][j][0], observer[i][j][1], row['latitude'], row['longitude'], row['altitude'])
+                # print(distance)
+                tmp_distance.append(distance)
+                tmp.loc[index, 'distance'] = distance
+            
+            #----- calculate L_dB from powersetting & distance -> copy to model_data -----#
+            npd_after_inter = npd_param.reindex(npd_param.index.union(tmp_distance))
+            npd_after_inter = npd_after_inter.interpolate(method='index',limit_direction='both',limit=200)
+            model_data = npd_after_inter.copy()
+            
+            #----- add L_dB to df['sound'] & select -----#
+            for index, row in tmp.iterrows():
+                tmp.loc[index,'sound'] = model_data.loc[row.distance,row['Power Setting']]
+            
+            #----- Single Sound Event Model -----#
+            if tmp.empty:
+                continue
+            tmp['Function'] = tmp['sound'].apply(function_L)
+            area = integrate.simpson(tmp['Function'])
+            #----- change -inf -> 0 -----#
+            if area == 0:
+                lSEL = 0
+            else:
+                lSEL = 10*np.log10( (1/1)*area)
+            
+            #----- add l_dB to observer -----#
+            observer[i][j][2] = lSEL
+            
+    return observer
+
+def generate_grid(flight, db):
+    df = {}
+
+    result_func = {
+    'status':1,
+    'id':flight
+    }
+    
+    doc_ref = db.collection('detail_and_grid').document('detail').collection(f'{flight}')
+    docs = list(doc_ref.stream())
+    flight_dict = list(map(lambda x: x.to_dict(), docs))
+    df['df'] = pd.DataFrame(flight_dict)
+    
+    #----- set air_type, NPD_ID -----#
+    aircraft = flight.split('-')[0]
+    # print(aircraft)
+    doc = db.collection('aircraft_engine').document(f'{aircraft}').get()
+    if doc.exists:
+        doc = doc.to_dict()
+        air_type = doc['type']
+        df['ACFT_ID'] = air_type
+        # print(air_type)
+        # doc2 = db.collection('NPD_ID').document(f'{air_type}').get()
+        # if doc2.exists:
+        #     doc2 = doc2.to_dict()
+        #     df['NPD_ID'] = doc2['NPD_ID']
+        # else: 
+        #     print('not found NPD : ', flight, ' Aircraf_type : ', air_type)
+        #     return False
+    else: 
+        # print('not found',aircraft)
+        response = requests.get(apiUrl + f"flights/{aircraft}",headers=auth_header)
+
+        if response.status_code == 200:
+            # print("got aircraft_type")
+            aircraft_type = response.json()['flights'][0]['aircraft_type']
+            
+            #----- get engine data from aircraft_type -----#
+            response = requests.get(apiUrl + f"aircraft/types/{aircraft_type}",headers=auth_header)
+            
+            if response.status_code == 200:
+                # print("got engine data")
+                engine_data = response.json()
+                engine_data.pop('manufacturer', None)
+                engine_data.pop('description', None)
+                engine_data['flight_id'] = aircraft
+                
+                #----- add to firestore -----#
+                doc_ref = db.collection(f'aircraft_engine').document(f'{aircraft}')
+                doc_ref.set(engine_data)
+            else:
+                result_func['status'] =0
+                result_func['message'] = 'Error executing request'
+                return result_func  
+                
+            df['ACFT_ID'] = aircraft
+
+        else:
+            result_func['message'] = 'Error executing request'
+            result_func['status'] =0
+            return result_func
+            
+    airtype = df['ACFT_ID']       
+    doc2 = db.collection('NPD_ID').document(f'{airtype}').get()
+    if doc2.exists:
+        doc2 = doc2.to_dict()
+        df['NPD_ID'] = doc2['NPD_ID']
+    else: 
+        # print('not found NPD : ', flight, f' Aircraf_type : \"{airtype}\"')
+        result_func['status'] =0
+        result_func['message'] = f'not found NPD : {flight}, Aircraf_type : "{airtype}"'
+        return result_func
+    NPDForCheck = df['NPD_ID']
+        
+    # print(f'airtype :{airtype}, npd: {NPDForCheck}, flight_ID: {flight}')
+    result_func['message'] = f'airtype :{airtype}, npd: {NPDForCheck}, flight_ID: {flight}'
+    # return result_func
+    
+    #----- set period -----#
+    doc2 = db.collection('filter_flight').document(f'{flight}').get()
+    if doc2.exists:
+        doc2 = doc2.to_dict()
+        period = doc2['period']
+        df['period'] = period
+    
+    #----- add data_fix_point -----#
+    aircraft_type = df["ACFT_ID"]
+    if aircraft_type in change_type:
+        aircraft_type = change_type[aircraft_type]
+
+    df['df_fix_point'] = df_fix_point[df_fix_point.ACFT_ID == aircraft_type]
+    df['df_fix_point'] = df['df_fix_point'].reset_index(drop=True)
+    
+    #----- add power setting to df -----#
+    df['df'] = add_powersetting_2_flight(df['df'], df['df_fix_point'])
+    
+    #----- get NPD table -----#
+    df['npd'] = npd_inter_per_flight(df['df'], df['NPD_ID'])
+    
+    #----- create grid [lat, long, 0] -----#
+    df['grid'] = create_observerer()
+    
+    #----- get grid from calculate_Grid -----#
+    df['grid'] = Calculate_Grid(df['grid'], df['df'], df['npd'])
+    
+    #----- TO PIVOT TABLE -----#
+    df_tmp = pd.DataFrame(columns = ['Lat','Long','L_dB'])
+
+    #----- matrix to pandas -----#
+    for i in range(len(df['grid'])):
+        row = pd.DataFrame(df['grid'][i], columns = ['Lat','Long','L_dB'])
+        df_tmp = pd.concat([df_tmp, row])
+    
+    #----- Create pivot table -----#
+    tabal_df = pd.pivot_table(df_tmp,index='Lat',columns='Long',values='L_dB')
+    df['grid'] = tabal_df
+    df['grid'] = df['grid'].iloc[::-1]
+    
+    result_func['df'] = df
+    return result_func
+# generate_grid(mockup_api)
+
+
+# result_func = {
+#     'status':1,
+#     'id':'test',
+#     'df':pd.DataFrame
+# }
--- Backend/main.py
+++ Backend/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 import pandas as pd
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 import requests
 import numpy as np
 import firebase_admin
@@ -14,6 +14,9 @@ from typing import Union
 import datetime
 import os
 import glob
+
+from function_backend.grid_to_firebase import generate_grid
+
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
@@ -23,6 +26,7 @@ app = FastAPI()
 class date_flight(BaseModel):
     start: str
     end: str
+    time: int
 
 class api_flightID(BaseModel):
     flight_id:Union[str, None] = None
@@ -31,6 +35,24 @@ class api_flightID(BaseModel):
 
 class list_flight(BaseModel):
     flights:Union[list, None] = None
+    duration_day:Union[int, None] = None
+    duration_night:Union[int, None] = None
+
+class DateRange(BaseModel):
+    start_date: datetime.date
+    end_date: datetime.date
+    hour: str
+
+class generate_grid_api(BaseModel):
+    date: str
+    
+    @validator("date")
+    def check_date_format(cls, value):
+        try:
+            datetime.datetime.strptime(value, '%Y-%m-%d')
+            return value
+        except ValueError:
+            raise ValueError("Incorrect date format. Use YYYY-MM-DD")
 
 
 
@@ -53,7 +75,7 @@ def validate(date_text):
         try:
             # print(date_text)
             test = datetime.datetime.strptime(date_text, '%Y-%m-%d %H:%M:%S')
-            print(test.timestamp())
+            # print(test.timestamp())
             return 0, test.timestamp()
             # year, month, day = date_text.split('-')
 
@@ -65,6 +87,8 @@ def validate(date_text):
         except ValueError:
             # raise ValueError("Incorrect data format, should be YYYY-MM-DD")
             return 1 ,"Incorrect data format, should be YYYY-MM-DD H:M:S"
+
+
 
 
 
@@ -86,22 +110,154 @@ def get_flight():
         }
 
 
+@app.post("/test")
+def create_date_range(date_range: DateRange):
+    start_date = date_range.start_date
+    end_date = date_range.end_date
+    hour = date_range.hour
+    if len(hour) == 1:
+        # Add zero-padding to the hour string
+        hour = f"0{hour}"
+    if int(hour) > 20:
+        end_hour = '00'
+    else:
+        end_hour = f'{int(hour)+3}'
+    if len(end_hour) == 1:
+        # Add zero-padding to the hour string
+        end_hour = f"0{end_hour}"
+    Airport_id = 'DMK'
+    Airport_start = f'{start_date}T{hour}%3A00%3A00Z'
+    Airport_end = f'{end_date}T{end_hour}%3A00%3A00Z'
+    #----- get flight data from DMK airport -----#
+    response = requests.get(apiUrl + f"airports/{Airport_id}/flights/departures?start={Airport_start}&end={Airport_end}",
+    headers=auth_header)
+        
+
+    Departures_FlightID = []
+    counter = 0
+    for i in response.json()['departures']:
+        if counter == 3:
+            break
+        counter += 1
+        if 'schedule' in i['fa_flight_id']:
+            Departures_FlightID.append(i['fa_flight_id'])
+    # print(Departures_FlightID)
+
+    #----- get data from flight ID -----#
+    Dict_departures_flights = {
+        'day':{},
+        'night':{}
+    }
+
+    for id in Departures_FlightID:
+        response = requests.get(apiUrl + f"flights/{id}/track",
+        headers=auth_header)
+
+        if response.status_code == 200:
+            # print(i)
+            #----- Add data to dict -----#
+            tmp_df = pd.DataFrame.from_dict(response.json()['positions'])
+            
+            #----- check time -----#
+            tmp_df = tmp_df.drop_duplicates(subset=['timestamp'], keep='first')
+            
+            #----- altitude <= 100k ft. & index[1]-index[0]==1 -----#
+            tmp_df = tmp_df[tmp_df['altitude']<=100]
+            tmp_df['check_index_1'] = tmp_df.index
+            tmp_df['check_index_2'] = tmp_df['check_index_1'].shift(-1)
+            tmp_df['drop'] = tmp_df['check_index_2']-tmp_df['check_index_1'] == 1
+            
+            #----- loop append only Departures -----#
+            res = pd.DataFrame()
+            n = 0
+            while(tmp_df.iloc[n]['drop']) :
+                res = res.append(tmp_df.iloc[n])
+                # res = pd.concat([res, tmp_df.iloc[n]])
+                n+=1
+            res = res.append(tmp_df.iloc[n])
+            # res = pd.concat([res, tmp_df.iloc[n]])
+            
+            #----- interpolate 1 sec -----#
+            res['timestamp'] = pd.to_datetime(res.timestamp)
+            nidx = np.arange(res['timestamp'][0], res['timestamp'].iloc[-1], 1000000 )
+            nidx = pd.to_datetime(nidx)
+
+            res['timestamp'] = res['timestamp'].round('S').dt.tz_localize(None)
+            res.set_index('timestamp', inplace=True)
+            res = res.reindex(res.index.union(nidx))
+
+            #----- interpolate ['altitude', 'groundspeed', 'heading', 'latitude', 'longitude'] -----#
+            res = res[['fa_flight_id', 'altitude', 'groundspeed', 'heading', 'latitude', 'longitude']]
+            res['altitude'] = pd.to_numeric(res['altitude'])
+            res['groundspeed'] = pd.to_numeric(res['groundspeed'])
+            res['heading'] = pd.to_numeric(res['heading'])
+            res = res.interpolate(method='time',limit_direction='both',limit=100)
+            res['fa_flight_id'] = id
+            
+            #----- ONLY TEST -----#
+            # res.to_csv(f'../Preprocess/ONLY_TEST/{id}.csv')
+            
+            #----- check day/night -----#
+            if res.iloc[:1].between_time('00:00', '16:00').empty :
+                # print(id,' night ', res.iloc[0])
+                Dict_departures_flights['night'][id] = res.copy()
+                Dict_departures_flights['night'][id].reset_index(inplace=True)
+                Dict_departures_flights['night'][id].rename(columns={'index':'timestamp'},inplace=True)
+            else :
+                # print(id,' day ', res.iloc[0])
+                Dict_departures_flights['day'][id] = res.copy()
+                Dict_departures_flights['day'][id].reset_index(inplace=True)
+                Dict_departures_flights['day'][id].rename(columns={'index':'timestamp'},inplace=True)
+                
+        else:
+            print("Error executing request")
+            
+        # i+=1
+        time.sleep(7)
+
+    # print(Dict_departures_flights)
+    for day_night in Dict_departures_flights:
+        for flight in Dict_departures_flights[day_night]:
+            # print(day_night, flight)
+
+            doc_ref = db.collection(f'filter_flight').document(f'{flight}')
+            doc_ref2 = db.collection(f'detail_and_grid').document(u'detail').collection(f'{flight}')
+            # print({
+            #     'date': int(Dict_departures_flights[day_night][flight].iloc[0].timestamp.floor(freq='H').timestamp()),
+            #     'id':id,
+            #     'period':day_night
+            # })
+            doc_ref.set({
+                'date': int(Dict_departures_flights[day_night][flight].iloc[0].timestamp.floor(freq='H').timestamp()),
+                'id':id,
+                'period':day_night
+            })
+            postdata = Dict_departures_flights[day_night][flight].to_dict('records')
+            list(map(lambda x: doc_ref2.add(x), postdata))
+            print('insert success', flight)
+       
+
+    # return {"response": f'Insert successful','status_code':200}
+    return {"None":Dict_departures_flights}
+
+
 @app.post('/get_flightaware')
 def get_flightaware(item:date_flight):
     # format yyyy-mm-dd
     start = item.start
     end = item.end
+    hour = item.time
     if len(start.split('-')) != 3 or len(end.split('-')) != 3 :
         return {'response': 'please check your format yyyy-mm-dd', 'status_code': 400}
     
     Airport_id = 'DMK'
-    Airport_start = f'{start}T00%3A00%3A00Z'
-    Airport_end = f'{end}T00%3A00%3A00Z'
+    Airport_start = f'{start}T{hour}%3A00%3A00Z'
+    Airport_end = f'{end}T{hour}%3A00%3A00Z'
     
     #----- get flight data from DMK airport -----#
     response = requests.get(apiUrl + f"airports/{Airport_id}/flights?start={Airport_start}&end={Airport_end}",
     headers=auth_header)
-        
+
     Arrivals_FlightID = []
     Departures_FlightID = []
     
@@ -118,10 +274,6 @@ def get_flightaware(item:date_flight):
         
     #----- get data from flight ID -----#
     Dict_departures_flights = {
-        'day':{},
-        'night':{}
-    }
-    Dict_arrivals_flights = {
         'day':{},
         'night':{}
     }
@@ -177,12 +329,12 @@ def get_flightaware(item:date_flight):
             
             #----- check day/night -----#
             if res.iloc[:1].between_time('07:00', '23:00').empty :
-                print(id,' night ', res.iloc[0])
+                # print(id,' night ', res.iloc[0])
                 Dict_departures_flights['night'][id] = res.copy()
                 Dict_departures_flights['night'][id].reset_index(inplace=True)
                 Dict_departures_flights['night'][id].rename(columns={'index':'timestamp'},inplace=True)
             else :
-                print(id,' day ', res.iloc[0])
+                # print(id,' day ', res.iloc[0])
                 Dict_departures_flights['day'][id] = res.copy()
                 Dict_departures_flights['day'][id].reset_index(inplace=True)
                 Dict_departures_flights['day'][id].rename(columns={'index':'timestamp'},inplace=True)
@@ -272,38 +424,106 @@ def filter_flight(item:api_flightID):
     flight_dict = list(map(lambda x: x.to_dict(), docs))
     return {'res':flight_dict}
 
+
+@app.post('/grid_to_firebase')
+def grid2firebase(date:generate_grid_api):
+    date_object = datetime.datetime.strptime(date.date, '%Y-%m-%d')
+    timestart = date_object.timestamp()
+    timeEnd = (date_object + datetime.timedelta(days=1) ).timestamp()
+    doc_ref = db.collection('filter_flight').where('date','>=',timestart).where('date','<',timeEnd)
+    docs = list(doc_ref.stream())
+    flight_dict = list(map(lambda x: x.to_dict(), docs))
+    flight_list = []
+    result = {
+        'error @ NPD ACFT':[],
+        'succuss':[]
+    }
+    for i in flight_dict:
+        tmp = generate_grid(i['id'], db)
+        if tmp['status'] == 0:
+            result['error @ NPD ACFT'].append(tmp)
+        else:
+            result['succuss'].append(tmp)
+        break
+        # if result[''] == False:
+        #     continue
+
+    # path = '../Preprocess/grid_flights/'
+    # csv_files = glob.glob(os.path.join(path, "*.csv"))
+    # for f in csv_files:
+    #     print(f)
+
+    #     if os.name == 'nt':
+    #         filename = f.split('\\')[1]
+    #     else:
+    #         filename = f.split('/')[3]
+        
+    #     flight_id = filename[:-4]
+    #     print(flight_id)
+    #     doc_ref = db.collection('detail_and_grid').document('grid').collection(f'{flight_id}')
+    #     check_exist = doc_ref.limit(1).stream()
+    #     check_exist = list(check_exist)
+    #     if check_exist != []:
+    #         print('found')
+    #         continue
+    #     else:
+    #         print('not found',flight_id)
+
+    #         df = pd.read_csv(f)
+    #         # print(df)
+
+    #         postdata = df.to_dict('records')
+
+    #         list(map(lambda x: doc_ref.add(x), postdata))
+    #         print(f'add {flight_id} succuess')
+
+    return {'response':result}
+
+
+
+
 @app.post('/flight_path')
 def flight_path(item:list_flight):
-    for i in item.flights:
-        print(i)
+    # print(item.duration_day, item.duration_night)
+    # for i in item.flights:
+    #     print(i)
     flight_list = item.flights
     res_list = []
+    #----- result grid for plot -----#
+    cumu_grid = [[0 for x in range(5)] for y in range(5)]
+    
     for id in flight_list:
         doc_ref2 = db.collection('detail_and_grid').document('detail').collection(f'{id}').order_by('timestamp')
         docs = list(doc_ref2.stream())
         flight_dict = list(map(lambda x: x.to_dict(), docs))
         df = pd.DataFrame(flight_dict)
+        df['altitude'] = df['altitude']*100
         df = df[['longitude','latitude','altitude']].values.tolist()
 
         doc_ref3 = db.collection('detail_and_grid').document('grid').collection(f'{id}')
         docs2 = list(doc_ref3.stream())
         flight_dict2 = list(map(lambda x: x.to_dict(), docs2))
         df2 = pd.DataFrame(flight_dict2)
-        # print(df2)
-        df2 = pd.melt(df2, id_vars=['Lat'], value_vars=df2.drop(columns=['Lat']).columns)
-        print(df2)
-        df2.rename(columns={'variable':'Long'},inplace=True)
-        df2['value'] = df2['value']/100
-        df2 = df2[['Long','Lat','value']].values.tolist()
-        # print(df)
-        # print(df2)
+        
+        if not df2.empty:
+            df2 = pd.melt(df2, id_vars=['Lat'], value_vars=df2.drop(columns=['Lat']).columns)
+            # print(df2)
+            df2.rename(columns={'variable':'Long'},inplace=True)
+            # df2['value'] = df2['value']/100
+            df2 = df2[['Long','Lat','value']].values.tolist()
+        else:
+            # print('empty : ',id)
+            # print(df2)
+            df2 = df2.values.tolist()
         tmp_dict ={
             'id':id,
             'value':df,
             'grid':df2
         }
         res_list.append(tmp_dict)
-    return {'response':'success','res':res_list}
+        
+        
+    return {'response':'success','res':res_list, 'cumu_grid':cumu_grid}
 
 
 @app.get('/csv_2_db')
@@ -344,27 +564,38 @@ def csv_2_db():
 
 
 
-@app.post('/grid_to_firebase')
-def grid2firebase():
-    path = '../Preprocess/grid_flights/'
-    csv_files = glob.glob(os.path.join(path, "*.csv"))
-    for f in csv_files:
-        df = pd.read_csv(f)
-        # print(df)
-        if os.name == 'nt':
-            filename = f.split('\\')[1]
-        else:
-            filename = f.split('/')[3]
+# @app.post('/grid_to_firebase')
+# def grid2firebase():
+#     path = '../Preprocess/grid_flights/'
+#     csv_files = glob.glob(os.path.join(path, "*.csv"))
+#     for f in csv_files:
+#         print(f)
+
+#         if os.name == 'nt':
+#             filename = f.split('\\')[1]
+#         else:
+#             filename = f.split('/')[3]
         
-        flight_id = filename[:-4]
+#         flight_id = filename[:-4]
+#         print(flight_id)
+#         doc_ref = db.collection('detail_and_grid').document('grid').collection(f'{flight_id}')
+#         check_exist = doc_ref.limit(1).stream()
+#         check_exist = list(check_exist)
+#         if check_exist != []:
+#             print('found')
+#             continue
+#         else:
+#             print('not found',flight_id)
 
-        doc_ref = db.collection('detail_and_grid').document('grid').collection(f'{flight_id}')
-        postdata = df.to_dict('records')
-        # print(postdata[0])
-        list(map(lambda x: doc_ref.add(x), postdata))
-        print(f'add {flight_id} succuess')
+#             df = pd.read_csv(f)
+#             # print(df)
 
-    return {'response':'successful'}
+#             postdata = df.to_dict('records')
+
+#             list(map(lambda x: doc_ref.add(x), postdata))
+#             print(f'add {flight_id} succuess')
+
+#     return {'response':'successful'}
 
 
 @app.get('/test_get_grid')
